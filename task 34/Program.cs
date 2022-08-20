ec6c208c99764fbd17dab055d47b0f4376a58393
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-21 00:55:56 +0400

    add task 34

---
+++ task 34/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] number = { 345, 897, 568, 234 };
+int i = 0;
+int Even = 0;
+double num = 10;
+do
+{
+    num = number[i];
+    num = num / 2;
+    if (num == (int)num)
+    {
+        Even++;
+    }
+    i++;
+} while (i <= number.Length - 1);
+Console.WriteLine(Even);
