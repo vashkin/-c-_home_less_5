9ae4ae425985b7e2d300b26e162b11d11651c309
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-21 01:09:19 +0400

    add task 36

---
+++ task 36/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] number = { -4, -6, 89, 6 };
+
+int i = 0;
+double num = 10;
+int notEven = 0;
+do
+{
+    num = i;
+    num = num / 2;
+    if (num != (int)num)
+    {
+        notEven = notEven + number[i];
+    }
+    i++;
+} while (i <= number.Length - 1);
+Console.WriteLine(notEven);
